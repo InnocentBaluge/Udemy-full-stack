3a57be205458f886e77e5ad9a97556e58f0ac28f
InnocentBaluge <innocent.baluge@yahoo.com>
2022-08-02 00:21:15 +0300

    second commit

--- connection.py
+++ connection.py
@@ -1,22 +1,27 @@
 import psycopg2
 
-
-connection = psycopg2.connect('dbname= innocent')
+connection = psycopg2.connect('dbname=innocent')
 
 cursor = connection.cursor()
 
-cursor.execute(
+cursor.execute('DROP TABLE IF EXISTS table1;')
 
-    '''
-        CREATE TABLE table1(
-            id INTEGER PRIMARY KEY,
-            completed BOOLEAN NOT NULL DEFAULT FALSE
-        );
-    '''
-)
+cursor.execute('''
+  CREATE TABLE table1 (
+    id INTEGER PRIMARY KEY,
+    completed BOOLEAN NOT NULL DEFAULT False
+  );
+''')
 
+cursor.execute('INSERT INTO table1 (id, completed) VALUES (%s, %s);', (1, True))
 
-cursor.execute('INSERT INTO  table1(id,completed) VALUES(1,true);')
+SQL = 'INSERT INTO table1 (id, completed) VALUES (%(id)s, %(completed)s);'
+
+data = {
+  'id': 2,
+  'completed': False
+}
+cursor.execute(SQL, data)
 
 connection.commit()
 
